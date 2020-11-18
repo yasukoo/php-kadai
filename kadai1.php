ba1699a72fc126fa3a81c1fd978f675c686ac416
yasukocchi <yasukochi.sp@gmail.com>
2020-11-18 11:55:40 +0000

    課題を修正しました

--- kadai1.php
+++ kadai1.php
@@ -2,26 +2,22 @@
 //1
 $a =3;
 $b =7;
-echo $a + $b;
-echo "\n";
+echo $a + $b . "\n";
 
 //2
 $array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7", "8月", "9月", "10月", "11月", "12月"];
-echo $array_month[7];
-echo "\n";
+echo $array_month[7] . "\n";
 
 //3
 $hello = "Hello,";
 $name = "安河内真平";
 $world = "'s World!";
-echo $hello. $name. $world;
-echo "\n";
+echo $hello. $name. $world . "\n";
 
 //4
 $tech_boost = "tech";
 $tech_boost .= " boost";
-echo $tech_boost;
-echo "\n";
+echo $tech_boost . "\n";
 
 //5
 $calendar_2018 = [
